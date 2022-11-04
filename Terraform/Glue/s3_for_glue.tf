09edb8fbbd5f9970e484770241ff112b10fc2c38
Ryan Viglione <86791607+ViggoMode2021@users.noreply.github.com>
2022-11-03 21:51:05 -0400

    Update s3_for_glue.tf

--- Terraform/Glue/s3_for_glue.tf
+++ Terraform/Glue/s3_for_glue.tf
@@ -1,38 +1,38 @@
-resource "aws_s3_bucket" "ryan-glue-aws" {
-  bucket = "ryan-glue-aws"
+resource "aws_s3_bucket" "s3_bucket_name" {
+  bucket = var.s3_bucket_name
 }
 
 resource "aws_s3_object" "temp-dir" {
-  bucket = aws_s3_bucket.ryan-glue-aws.id
+  bucket = aws_s3_bucket.s3_bucket_name.id
   key    = "temp-dir/"
 }
 
 resource "aws_s3_object" "scripts" {
-  bucket = aws_s3_bucket.ryan-glue-aws.id
+  bucket = aws_s3_bucket.s3_bucket_name.id
   key    = "scripts/"
 }
 
 resource "aws_s3_object" "data" {
-  bucket = aws_s3_bucket.ryan-glue-aws.id
+  bucket = aws_s3_bucket.s3_bucket_name.id
   key    = "data/"
 }
 
 resource "aws_s3_object" "customers_database" {
-  bucket = aws_s3_bucket.ryan-glue-aws.id
+  bucket = aws_s3_bucket.s3_bucket_name.id
   key    = "data/customers_database/"
 }
 
 resource "aws_s3_object" "customers_csv" {
-  bucket = aws_s3_bucket.ryan-glue-aws.id
+  bucket = aws_s3_bucket.s3_bucket_name.id
   key    = "data/customers_database/customers_csv/"
 }
 
 resource "aws_s3_object" "dataload-20221103" {
-  bucket = aws_s3_bucket.ryan-glue-aws.id
+  bucket = aws_s3_bucket.s3_bucket_name.id
   key    = "data/customers_database/customers_csv/dataload-20221103/"
 }
 
 resource "aws_s3_object" "athena_results" {
-  bucket = aws_s3_bucket.ryan-glue-aws.id
+  bucket = aws_s3_bucket.s3_bucket_name.id
   key    = "athena_results/"
 }
