a9ee0ac821e59441383e06c2d29205df702a2614
Ryan Viglione <86791607+ViggoMode2021@users.noreply.github.com>
2022-11-15 19:53:34 -0500

    Update main.tf

--- Terraform/Glue-Api-Gateway/rest_api_module/main.tf
+++ Terraform/Glue-Api-Gateway/rest_api_module/main.tf
@@ -2,14 +2,14 @@ provider "aws" {
   region = "us-east-1"
 }
 
-data "aws_s3_bucket_object" "lambda" {
+data "aws_s3_object" "lambda" {
   bucket = "api-gateway-lambda-zip-files"
   key    = "best_albums_rest_api_lambda.zip"
 }
 
 resource "aws_lambda_function" "Best_Albums_API_Gateway" {
-  s3_bucket     = data.aws_s3_bucket_object.lambda.bucket
-  s3_key        = data.aws_s3_bucket_object.lambda.key
+  s3_bucket     = data.aws_s3_object.lambda.bucket
+  s3_key        = data.aws_s3_object.lambda.key
   function_name = "best_albums_rest_api_lambda"
   handler       = "main.lambda_handler"
   runtime       = "python3.9"
@@ -21,101 +21,101 @@ resource "aws_lambda_function" "Best_Albums_API_Gateway" {
 resource "aws_lambda_permission" "allow_api" {
   statement_id  = "AllowAPIgatewayInvokation"
   action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.Ryan_API_Gateway.function_name
+  function_name = aws_lambda_function.Best_Albums_API_Gateway.function_name
   principal     = "apigateway.amazonaws.com"
 }
 
 ################ API GATEWAY ################
 
-resource "aws_api_gateway_rest_api" "test-Ryan-api-gateway" {
-  name = "test-Ryan-api-gateway"
+resource "aws_api_gateway_rest_api" "best_albums_API" {
+  name = "best_albums_API"
   endpoint_configuration {
     types = ["REGIONAL"]
   }
 }
 
-resource "aws_api_gateway_resource" "product" {
-  rest_api_id = aws_api_gateway_rest_api.test-Ryan-api-gateway.id
-  parent_id   = aws_api_gateway_rest_api.test-Ryan-api-gateway.root_resource_id
-  path_part   = "product"
+resource "aws_api_gateway_resource" "album" {
+  rest_api_id = aws_api_gateway_rest_api.best_albums_API.id
+  parent_id   = aws_api_gateway_rest_api.best_albums_API.root_resource_id
+  path_part   = "album"
 }
 
 // POST
 resource "aws_api_gateway_method" "POST" {
-  rest_api_id      = aws_api_gateway_rest_api.test-Ryan-api-gateway.id
-  resource_id      = aws_api_gateway_resource.product.id
+  rest_api_id      = aws_api_gateway_rest_api.best_albums_API.id
+  resource_id      = aws_api_gateway_resource.album.id
   http_method      = "POST"
   authorization    = "NONE"
   api_key_required = false
 }
 
 resource "aws_api_gateway_integration" "integration" {
-  rest_api_id             = aws_api_gateway_rest_api.test-Ryan-api-gateway.id
-  resource_id             = aws_api_gateway_resource.product.id
+  rest_api_id             = aws_api_gateway_rest_api.best_albums_API.id
+  resource_id             = aws_api_gateway_resource.album.id
   http_method             = aws_api_gateway_method.POST.http_method
   integration_http_method = "POST"
   type                    = "AWS_PROXY"
-  uri                     = aws_lambda_function.Ryan_API_Gateway.invoke_arn
+  uri                     = aws_lambda_function.Best_Albums_API_Gateway.invoke_arn
 }
 
 // GET
 resource "aws_api_gateway_method" "GET" {
-  rest_api_id      = aws_api_gateway_rest_api.test-Ryan-api-gateway.id
-  resource_id      = aws_api_gateway_resource.product.id
+  rest_api_id      = aws_api_gateway_rest_api.best_albums_API.id
+  resource_id      = aws_api_gateway_resource.album.id
   http_method      = "GET"
   authorization    = "NONE"
   api_key_required = false
 }
 
 resource "aws_api_gateway_integration" "integration-get" {
-  rest_api_id             = aws_api_gateway_rest_api.test-Ryan-api-gateway.id
-  resource_id             = aws_api_gateway_resource.product.id
+  rest_api_id             = aws_api_gateway_rest_api.best_albums_API.id
+  resource_id             = aws_api_gateway_resource.album.id
   http_method             = aws_api_gateway_method.GET.http_method
   integration_http_method = "POST"
   type                    = "AWS_PROXY"
-  uri                     = aws_lambda_function.Ryan_API_Gateway.invoke_arn
+  uri                     = aws_lambda_function.Best_Albums_API_Gateway.invoke_arn
 }
 
 // PATCH
 resource "aws_api_gateway_method" "PATCH" {
-  rest_api_id      = aws_api_gateway_rest_api.test-Ryan-api-gateway.id
-  resource_id      = aws_api_gateway_resource.product.id
+  rest_api_id      = aws_api_gateway_rest_api.best_albums_API.id
+  resource_id      = aws_api_gateway_resource.album.id
   http_method      = "PATCH"
   authorization    = "NONE"
   api_key_required = false
 }
 
 resource "aws_api_gateway_integration" "integration-patch" {
-  rest_api_id             = aws_api_gateway_rest_api.test-Ryan-api-gateway.id
-  resource_id             = aws_api_gateway_resource.product.id
+  rest_api_id             = aws_api_gateway_rest_api.best_albums_API.id
+  resource_id             = aws_api_gateway_resource.album.id
   http_method             = aws_api_gateway_method.PATCH.http_method
   integration_http_method = "POST"
   type                    = "AWS_PROXY"
-  uri                     = aws_lambda_function.Ryan_API_Gateway.invoke_arn
+  uri                     = aws_lambda_function.Best_Albums_API_Gateway.invoke_arn
 }
 
 // DELETE
 resource "aws_api_gateway_method" "DELETE" {
-  rest_api_id      = aws_api_gateway_rest_api.test-Ryan-api-gateway.id
-  resource_id      = aws_api_gateway_resource.product.id
+  rest_api_id      = aws_api_gateway_rest_api.best_albums_API.id
+  resource_id      = aws_api_gateway_resource.album.id
   http_method      = "DELETE"
   authorization    = "NONE"
   api_key_required = false
 }
 
 resource "aws_api_gateway_integration" "integration-delete" {
-  rest_api_id             = aws_api_gateway_rest_api.test-Ryan-api-gateway.id
-  resource_id             = aws_api_gateway_resource.product.id
+  rest_api_id             = aws_api_gateway_rest_api.best_albums_API.id
+  resource_id             = aws_api_gateway_resource.album.id
   http_method             = aws_api_gateway_method.DELETE.http_method
   integration_http_method = "POST"
   type                    = "AWS_PROXY"
-  uri                     = aws_lambda_function.Ryan_API_Gateway.invoke_arn
+  uri                     = aws_lambda_function.Best_Albums_API_Gateway.invoke_arn
 }
 
 ################ Deployment of API gateway ################
 
 resource "aws_api_gateway_deployment" "deployment1" {
-  rest_api_id = aws_api_gateway_rest_api.test-Ryan-api-gateway.id
+  rest_api_id = aws_api_gateway_rest_api.best_albums_API.id
 
   depends_on = [aws_api_gateway_integration.integration]
   lifecycle {
@@ -125,8 +125,8 @@ resource "aws_api_gateway_deployment" "deployment1" {
 
 resource "aws_api_gateway_stage" "example" {
   deployment_id = aws_api_gateway_deployment.deployment1.id
-  rest_api_id   = aws_api_gateway_rest_api.test-Ryan-api-gateway.id
+  rest_api_id   = aws_api_gateway_rest_api.best_albums_API.id
   stage_name    = "test"
 }
 
-output "complete_invoke_url" { value = "${aws_api_gateway_deployment.deployment1.invoke_url}${aws_api_gateway_stage.example.stage_name}/${aws_api_gateway_resource.product.path_part}" }
+output "complete_invoke_url" { value = "${aws_api_gateway_deployment.deployment1.invoke_url}${aws_api_gateway_stage.example.stage_name}/${aws_api_gateway_resource.album.path_part}" }
