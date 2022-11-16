64752fdbc2eb7155d9fa7dae4f3be109d17900d5
Ryan Viglione <86791607+ViggoMode2021@users.noreply.github.com>
2022-11-15 19:53:15 -0500

    Update main.tf

--- Terraform/Glue-Api-Gateway/main.tf
+++ Terraform/Glue-Api-Gateway/main.tf
@@ -1,132 +1,62 @@
-provider "aws" {
-  region = "us-east-1"
-}
+resource "aws_s3_bucket" "s3_bucket_name" {
+  bucket = "best-albums-glue"
+} # S3 bucket to store CSV data
 
-data "aws_s3_bucket_object" "lambda" {
-  bucket = "api-gateway-lambda-zip-files"
-  key    = "best_albums_rest_api_lambda.zip"
-}
+resource "aws_s3_bucket" "s3_bucket_name_2" {
+  bucket = "best-albums-glue-job-code"
+} # S3 bucket to store glue job Python code
 
-resource "aws_lambda_function" "Best_Albums_API_Gateway" {
-  s3_bucket     = data.aws_s3_bucket_object.lambda.bucket
-  s3_key        = data.aws_s3_bucket_object.lambda.key
-  function_name = "best_albums_rest_api_lambda"
-  handler       = "main.lambda_handler"
-  runtime       = "python3.9"
-  role          = "arn:aws:iam::583715230104:role/serverless-api-role"
-  memory_size   = 128
-  timeout       = 10
-}
+resource "aws_s3_object" "best-albums-data" {
 
-resource "aws_lambda_permission" "allow_api" {
-  statement_id  = "AllowAPIgatewayInvokation"
-  action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.Ryan_API_Gateway.function_name
-  principal     = "apigateway.amazonaws.com"
-}
+  bucket = aws_s3_bucket.s3_bucket_name.id
 
-################ API GATEWAY ################
+  key = "500_best_albums.csv"
 
-resource "aws_api_gateway_rest_api" "test-Ryan-api-gateway" {
-  name = "test-Ryan-api-gateway"
-  endpoint_configuration {
-    types = ["REGIONAL"]
+  source = "C:/Users/ryans/Desktop/500_best_albums.csv"
+
+} # S3 object for CSV data
+
+resource "aws_s3_object" "glue_job" { # CHANGE THIS 
+
+  bucket = aws_s3_bucket.s3_bucket_name_2.id
+
+  key = "glue_job.py"
+
+  source = "C:/Users/ryans/Desktop/ecuador_glue/glue_job.py"
+
+} # S3 object for Python file 
+
+resource "aws_glue_catalog_database" "aws_glue_catalog_database" {
+  name = "500_best_albums_database"
+} # Glue catalog database 
+
+resource "aws_glue_crawler" "pii_crawler_glue_db" {
+  database_name = aws_glue_catalog_database.aws_glue_catalog_database.name
+  name          = "500_albums_crawler"
+  role          = "arn:aws:iam::583715230104:role/glue-course-full-access"
+
+  configuration = jsonencode(
+    {
+      Grouping = {
+        TableGroupingPolicy = "CombineCompatibleSchemas"
+      }
+      CrawlerOutput = {
+        Partitions = { AddOrUpdateBehavior = "InheritFromTable" }
+      }
+      Version = 1
+    }
+  )
+
+  s3_target {
+    path = "s3://500-best-albums-glue"
   }
-}
+} # Glue crawler that will extract data from CSV and put it into a table in the Glue catalog database. 
 
-resource "aws_api_gateway_resource" "product" {
-  rest_api_id = aws_api_gateway_rest_api.test-Ryan-api-gateway.id
-  parent_id   = aws_api_gateway_rest_api.test-Ryan-api-gateway.root_resource_id
-  path_part   = "product"
-}
+resource "aws_glue_job" "albums-job" {
+  name     = "albums-job-terraform"
+  role_arn = "arn:aws:iam::583715230104:role/glue-course-full-access"
 
-// POST
-resource "aws_api_gateway_method" "POST" {
-  rest_api_id      = aws_api_gateway_rest_api.test-Ryan-api-gateway.id
-  resource_id      = aws_api_gateway_resource.product.id
-  http_method      = "POST"
-  authorization    = "NONE"
-  api_key_required = false
-}
-
-resource "aws_api_gateway_integration" "integration" {
-  rest_api_id             = aws_api_gateway_rest_api.test-Ryan-api-gateway.id
-  resource_id             = aws_api_gateway_resource.product.id
-  http_method             = aws_api_gateway_method.POST.http_method
-  integration_http_method = "POST"
-  type                    = "AWS_PROXY"
-  uri                     = aws_lambda_function.Ryan_API_Gateway.invoke_arn
-}
-
-// GET
-resource "aws_api_gateway_method" "GET" {
-  rest_api_id      = aws_api_gateway_rest_api.test-Ryan-api-gateway.id
-  resource_id      = aws_api_gateway_resource.product.id
-  http_method      = "GET"
-  authorization    = "NONE"
-  api_key_required = false
-}
-
-resource "aws_api_gateway_integration" "integration-get" {
-  rest_api_id             = aws_api_gateway_rest_api.test-Ryan-api-gateway.id
-  resource_id             = aws_api_gateway_resource.product.id
-  http_method             = aws_api_gateway_method.GET.http_method
-  integration_http_method = "POST"
-  type                    = "AWS_PROXY"
-  uri                     = aws_lambda_function.Ryan_API_Gateway.invoke_arn
-}
-
-// PATCH
-resource "aws_api_gateway_method" "PATCH" {
-  rest_api_id      = aws_api_gateway_rest_api.test-Ryan-api-gateway.id
-  resource_id      = aws_api_gateway_resource.product.id
-  http_method      = "PATCH"
-  authorization    = "NONE"
-  api_key_required = false
-}
-
-resource "aws_api_gateway_integration" "integration-patch" {
-  rest_api_id             = aws_api_gateway_rest_api.test-Ryan-api-gateway.id
-  resource_id             = aws_api_gateway_resource.product.id
-  http_method             = aws_api_gateway_method.PATCH.http_method
-  integration_http_method = "POST"
-  type                    = "AWS_PROXY"
-  uri                     = aws_lambda_function.Ryan_API_Gateway.invoke_arn
-}
-
-// DELETE
-resource "aws_api_gateway_method" "DELETE" {
-  rest_api_id      = aws_api_gateway_rest_api.test-Ryan-api-gateway.id
-  resource_id      = aws_api_gateway_resource.product.id
-  http_method      = "DELETE"
-  authorization    = "NONE"
-  api_key_required = false
-}
-
-resource "aws_api_gateway_integration" "integration-delete" {
-  rest_api_id             = aws_api_gateway_rest_api.test-Ryan-api-gateway.id
-  resource_id             = aws_api_gateway_resource.product.id
-  http_method             = aws_api_gateway_method.DELETE.http_method
-  integration_http_method = "POST"
-  type                    = "AWS_PROXY"
-  uri                     = aws_lambda_function.Ryan_API_Gateway.invoke_arn
-}
-
-################ Deployment of API gateway ################
-
-resource "aws_api_gateway_deployment" "deployment1" {
-  rest_api_id = aws_api_gateway_rest_api.test-Ryan-api-gateway.id
-
-  depends_on = [aws_api_gateway_integration.integration]
-  lifecycle {
-    create_before_destroy = true
+  command {
+    script_location = "s3://pii-glue-job-code/glue_job.py"
   }
-}
-
-resource "aws_api_gateway_stage" "example" {
-  deployment_id = aws_api_gateway_deployment.deployment1.id
-  rest_api_id   = aws_api_gateway_rest_api.test-Ryan-api-gateway.id
-  stage_name    = "test"
-}
-
-output "complete_invoke_url" { value = "${aws_api_gateway_deployment.deployment1.invoke_url}${aws_api_gateway_stage.example.stage_name}/${aws_api_gateway_resource.product.path_part}" }
+} # Reference to code for Glue job that is stored in S3 bucket.
