3209ef28a63aa5196e23b0951831960292b6fe1f
Ryan Viglione <86791607+ViggoMode2021@users.noreply.github.com>
2022-11-16 20:42:14 -0500

    Update main.tf

--- Terraform/Glue-Api-Gateway/dynamo_db_module/main.tf
+++ Terraform/Glue-Api-Gateway/dynamo_db_module/main.tf
@@ -12,10 +12,10 @@ resource "aws_dynamodb_table" "best_albums_table" {
   billing_mode   = "PROVISIONED"
   read_capacity  = 20
   write_capacity = 20
-  hash_key       = "album_id"
+  hash_key       = "title"
 
   attribute {
-    name = "album_id"
-    type = "N"
+    name = "title"
+    type = "S"
   }
 }
