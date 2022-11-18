c8dd6d4d03afee077b41a531e954a8ff8b6d6b14
Ryan Viglione <86791607+ViggoMode2021@users.noreply.github.com>
2022-11-17 19:35:11 -0500

    Update main.tf

--- Terraform/Glue-Api-Gateway/main.tf
+++ Terraform/Glue-Api-Gateway/main.tf
@@ -1,62 +1,18 @@
-resource "aws_s3_bucket" "s3_bucket_name" {
-  bucket = "best-albums-glue"
-} # S3 bucket to store CSV data
+#export AWS_ACCESS_KEY_ID=""
+#export AWS_SECRET_ACCESS_KEY=""
 
-resource "aws_s3_bucket" "s3_bucket_name_2" {
-  bucket = "best-albums-glue-job-code"
-} # S3 bucket to store glue job Python code
+provider "aws" {
+  region = "us-east-1"
+}
 
-resource "aws_s3_object" "best-albums-data" {
+module "rest_api_module" {
+  source = "C:/Users/ryans/Desktop/Terraform_ETL_Api_Gateway/rest_api_module"
+}
 
-  bucket = aws_s3_bucket.s3_bucket_name.id
+module "dynamo_db_module" {
+  source = "C:/Users/ryans/Desktop/Terraform_ETL_Api_Gateway/dynamo_db_module"
+}
 
-  key = "500_best_albums.csv"
-
-  source = "C:/Users/ryans/Desktop/500_best_albums.csv"
-
-} # S3 object for CSV data
-
-resource "aws_s3_object" "glue_job" { # CHANGE THIS 
-
-  bucket = aws_s3_bucket.s3_bucket_name_2.id
-
-  key = "glue_job.py"
-
-  source = "C:/Users/ryans/Desktop/ecuador_glue/glue_job.py"
-
-} # S3 object for Python file 
-
-resource "aws_glue_catalog_database" "aws_glue_catalog_database" {
-  name = "500_best_albums_database"
-} # Glue catalog database 
-
-resource "aws_glue_crawler" "pii_crawler_glue_db" {
-  database_name = aws_glue_catalog_database.aws_glue_catalog_database.name
-  name          = "500_albums_crawler"
-  role          = "arn:aws:iam::583715230104:role/glue-course-full-access"
-
-  configuration = jsonencode(
-    {
-      Grouping = {
-        TableGroupingPolicy = "CombineCompatibleSchemas"
-      }
-      CrawlerOutput = {
-        Partitions = { AddOrUpdateBehavior = "InheritFromTable" }
-      }
-      Version = 1
-    }
-  )
-
-  s3_target {
-    path = "s3://500-best-albums-glue"
-  }
-} # Glue crawler that will extract data from CSV and put it into a table in the Glue catalog database. 
-
-resource "aws_glue_job" "albums-job" {
-  name     = "albums-job-terraform"
-  role_arn = "arn:aws:iam::583715230104:role/glue-course-full-access"
-
-  command {
-    script_location = "s3://pii-glue-job-code/glue_job.py"
-  }
-} # Reference to code for Glue job that is stored in S3 bucket.
+module "glue_module" {
+  source = "C:/Users/ryans/Desktop/Terraform_ETL_Api_Gateway/glue_module"
+}
