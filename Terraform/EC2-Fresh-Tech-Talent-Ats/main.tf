a889a6b522017a799699b81cc49ef1301348473e
Ryan Viglione <86791607+ViggoMode2021@users.noreply.github.com>
2023-01-18 08:12:55 -0500

    Create main.tf

---
+++ Terraform/EC2-Fresh-Tech-Talent-Ats/main.tf
@@ -0,0 +1,24 @@
+data "aws_ami" "ubuntu" { # change to AWS Linux 2
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"] # change to Amazon Linux 2
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+resource "aws_instance" "web" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "HelloWorld"
+  }
+}
